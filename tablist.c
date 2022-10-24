de9b321bd0d4d1b25df839f14f763d5d5c837d36
maellegautrin <107635830+maellegautrin@users.noreply.github.com>
2022-10-24 19:40:32 +0200

    Add files via upload

--- tablist.c
+++ tablist.c
@@ -10,21 +10,45 @@ struct elem_t
 typedef struct elem_t elem_t;
 int MAX_CAP = 100;
 
-typedef elem_t tableau[MAX_CAP] tabl;
 
-structuct tab
+typedef elem_t tabl[100];
+
+struct tlist_t
 {
     tabl tab;
     int size;
     int first;
     int last;
-}
-int main(void)
+};
+
+typedef struct tlist_t tlist_t;
+
+tlist_t* tlist_news(void)
 {
-    tlist_t t = ;
-
-    printf("%d.\n", MAX_CAP);
-    return 0;
+    tlist_t t;
+    for(int i=0;i<MAX_CAP;++i){
+        t.tab[i].is_free=1;
+    }
+    t.size=0;
+    return t;
 }
 
-gcc 
+
+int tlist_free(tlist_t* l)
+{
+    int s=l.size;
+    l.first=0;
+    l.last=0;
+    l.size=0;
+    return s;
+}
+
+int tlist_size(tlist_t* l)
+{
+    return l.size;
+}
+
+tlist_t main(void)
+{
+    return tlist_news(void);
+}
