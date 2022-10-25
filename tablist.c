ad848d70b0f0293e6684e1ba844e3d43e2f49a3a
maellegautrin <107635830+maellegautrin@users.noreply.github.com>
2022-10-25 10:13:08 +0200

    Add files via upload

--- tablist.c
+++ tablist.c
@@ -1,4 +1,6 @@
-struct elem_t
+
+//question_1
+struct elem_t                                
 {
     int x;
     int y;
@@ -8,9 +10,11 @@ struct elem_t
 };
 
 typedef struct elem_t elem_t;
-int MAX_CAP = 100;
 
+//question_2:
+int MAX_CAP = 100;                        
 
+// question_3:
 typedef elem_t tabl[100];
 
 struct tlist_t
@@ -21,34 +25,85 @@ struct tlist_t
     int last;
 };
 
-typedef struct tlist_t tlist_t;
-
-tlist_t* tlist_news(void)
+typedef struct tlist_t tlist_t;    
+         
+//question_4:
+tlist_t* tlist_news()                        
 {
-    tlist_t t;
+    tlist_t* t;
     for(int i=0;i<MAX_CAP;++i){
-        t.tab[i].is_free=1;
+        t->tab[i].is_free=1;
     }
-    t.size=0;
+    t->size=0;
     return t;
 }
 
-
-int tlist_free(tlist_t* l)
+//question_5:
+int tlist_free(tlist_t* l)               
 {
-    int s=l.size;
-    l.first=0;
-    l.last=0;
-    l.size=0;
+    int s=l->size;
+    l->first=0;
+    l->last=0;
+    l->size=0;
     return s;
 }
 
+//question_6:
 int tlist_size(tlist_t* l)
 {
-    return l.size;
+    return l->size;
 }
 
-tlist_t main(void)
+//question_7:
+int tlist_add(tlist_t* l,int x, int y)
 {
-    return tlist_news(void);
+    if (l->last==MAX_CAP){
+        return 0;}
+    l->tab[l->last +1].x= x;
+    l->tab[l->last +1].y= y;
+    l->last +=1;
+    l->size +=1;
+    return 1;
+}
+
+
+//question_8:
+int tlist_remove(tlist_t* l, int x, int y){
+    for(int i= l->first;i<=l->last;i++){
+        if(l->tab[i].x==x && l->tab[i].y==y){
+            for(int j=i;j<l->last;i++){
+                l->tab[j]=l->tab[j+1];
+                l->last -=1;
+                l->size -=1;
+                return 1;
+            }
+        }
+    }
+}
+
+//question_9:
+int tlist_pop(tlist_t* l){
+    if(l->size==0){return 0;};
+    l->first +=1;
+    l->size -=1;
+    return 1;
+}
+
+//question_10:
+int tlist_top(tlist_t* l, int *x, int *y){
+    if(l->size==0){return 0;};
+    *x=l->tab[l->first].x;
+    *y=l->tab[l->first].y;
+}
+
+//question_11:
+int tlist_push(tlist_t* l, int x, int y){
+    
+}
+
+
+
+int main(void)
+{
+    return 0;
 }
