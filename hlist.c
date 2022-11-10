d07eb659c6a2d28c3adeb2e28a327d0762f18269
maellegautrin <107635830+maellegautrin@users.noreply.github.com>
2022-11-10 21:52:20 +0100

    Update hlist.c

--- hlist.c
+++ hlist.c
@@ -1,6 +1,6 @@
 
 //question_1:
-struct hnode_t 			// on définit un noeud tel que ce noeud en a un autre à gauche, un autre à droite, un autre en dessous, et un autre au dessus.
+typedef struct hnode_t hnode_t		// on définit un noeud tel que ce noeud en a un autre à gauche, un autre à droite, un autre en dessous, et un autre au dessus.
 {
 	hnode_t* next;   
 	hnode_t* prev;
@@ -27,7 +27,7 @@ typedef struct hlist_t hlist_t;
 
 hlist_t* hlist_new()
 {
-	hlist_t* h=malloc(sizeof(hlist_t));     //on alloue de la mémoire
+	hlist_t* t=malloc(sizeof(hlist_t));     //on alloue de la mémoire
 	t->head.moins_infini=1;			//on marque les valeur + et - l'infini dans la liste
 	t->head.next.plus_infini=1;
 	t->height=1;				// on initialise la taille à 1
@@ -56,17 +56,19 @@ int hlist_search(hlist_t *l, int v, hnode_t* path[])
 		if (compteur->valeur==v)                // si on trouve la valeur, on finit de remplir path avec les pointeurs vers notre valeur et on renvoi 1
 		{
 			for(c; c<l->height;c++)
-				path[c]=valeur;
-				compteur=compteur.dessous;
+			{
+				path[c]=v;
+				compteur=compteur->dessous;
+			}
 			return 1;
 		}
 		if (compteur->valeur<v)			// si on trouve une valeur plus petite que v, on se décale à droite
-			compteur=compteur.next;
+			compteur=compteur->next;
 		else					// sinon, on descend et on ajoute le pointeur du dernier élément plus petit à path.
 		{
-			path[c]=compteur.prev;
+			path[c]=compteur->prev;
 			c++;
-			compteur=compteur.prev.dessous;
+			compteur=compteur->prev.dessous;
 		}
 	}
 	return 0;
@@ -74,7 +76,7 @@ int hlist_search(hlist_t *l, int v, hnode_t* path[])
 
 //question_6:
 
-int hlist_add(hlist_remove(hlist_t *l, int v)
+int hlist_add(hlist_t *l, int v)
 {
 	hnode_t* path[]=malloc(sizeof(l->height));
 	if (hlist_search(l,v,path))
